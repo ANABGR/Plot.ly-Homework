e24bf26fcaf43cb2afd081246d7f3525d9ec6980
ANABGR <anabgr1@gmail.com>
2021-04-22 22:00:01 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,19 @@ function buildMetadata(sample) {
   });
 }
 
+
+
+//function buildGauge(wfreq) {}
+
+function buildCharts(sample) {
+
+// Use `d3.json` to fetch the sample data for the plots
+d3.json("samples.json").then((data) => {
+  var samples= data.samples;
+  var resultsarray= samples.filter(sampleobject => 
+      sampleobject.id == sample);
+  var result= resultsarray[0]
+
+  var ids = result.otu_ids;
+  var labels = result.otu_labels;
+  var values = result.sample_values;
